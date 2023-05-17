310a259787497ee5a50f76db62f497e1b24006a9
Jelena Basaric <jbasaric83@gmail.com>
2023-05-17 23:32:35 +0200

    create form and control in ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ProjectForm!:FormGroup;
+  ngOnInit(): void {
+    this.ProjectForm=new FormGroup({
+
+      'projectName':new FormControl(null,Validators.required),
+      'email':new FormControl(null,[Validators.required,Validators.email])
+
+    });
+    
+  }
 }
